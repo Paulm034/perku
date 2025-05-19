60b775825dcd9cb107adb4d37c928acdd495e8db
Paulm034 <105877265+Paulm034@users.noreply.github.com>
2025-05-19 23:26:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Слайдер работает');
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 5000);
